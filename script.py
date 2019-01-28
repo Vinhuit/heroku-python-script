f8ad857c463f1074388252cdf9407be84d64e7db
Vinh Huynh <caubequay00@gmail.com>
2019-01-28 15:13:00 +0700

    Update script.py

--- script.py
+++ script.py
@@ -155,6 +155,7 @@ def main():
 SimpleMonitor()
 #schedule.every(3).minutes.do(main)
 print(datetime.datetime.now())
+schedule.every(2).minutes.do(ping)
 schedule.every().day.at("21:00").do(startmain).tag('main')
 schedule.every().day.at("16:00").do(cancelschedule).tag('cancelmain')
 
